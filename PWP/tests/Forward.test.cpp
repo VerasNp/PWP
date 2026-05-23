09556582f026b34643c65dd550f11b4ef8fc9f1b
VerasNp <vitorveras@proton.me>
2026-05-23 12:22:13 -0300

    test: Adiciona testes da derivada progressiva

--- PWP/tests/Forward.test.cpp
+++ PWP/tests/Forward.test.cpp
@@ -43,24 +43,24 @@ TEST_CASE("Forward O(h)") {
     }
 }
 
-// TEST_CASE("Forward O(h2)") {
-//     const double xValue = 1.234;
-//     const double hValue = 1e-3;
+TEST_CASE("Forward O(h2)") {
+    const double xValue = 1.234;
+    const double hValue = 1e-3;
 
-//     SUBCASE("1a derivada") {
-//         Derivate forward(DerivativeOrder::First, ErrorOrder::O_h2);
-//         CHECK(forward.calculate(linear, xValue, hValue) ==
-//               doctest::Approx(3.0).epsilon(hValue * hValue));
-//     }
-// }
+    SUBCASE("1a derivada") {
+        Derivate forward(DerivativeOrder::First, ErrorOrder::O_h2);
+        CHECK(forward.calculate(linear, xValue, hValue) ==
+              doctest::Approx(3.0).epsilon(hValue * hValue));
+    }
+}
 
-// TEST_CASE("Forward O(h2) mais preciso que O(h)") {
-//     const double xValue = 1.234;
-//     const double hValue = 1e-3;
-//     Derivate forwardOh(DerivativeOrder::First, ErrorOrder::O_h);
-//     Derivate forwardOh2(DerivativeOrder::First, ErrorOrder::O_h2);
-//     const double exact = 2.0 * xValue;
-//     const double erro_oh = std::abs(forwardOh.calculate(quadratic, xValue, hValue) - exact);
-//     const double erro_oh2 = std::abs(forwardOh2.calculate(quadratic, xValue, hValue) - exact);
-//     CHECK(erro_oh2 < erro_oh);
-// }
+TEST_CASE("Forward O(h2) mais preciso que O(h)") {
+    const double xValue = 1.234;
+    const double hValue = 1e-3;
+    Derivate forwardOh(DerivativeOrder::First, ErrorOrder::O_h);
+    Derivate forwardOh2(DerivativeOrder::First, ErrorOrder::O_h2);
+    const double exact = 2.0 * xValue;
+    const double erro_oh = std::abs(forwardOh.calculate(quadratic, xValue, hValue) - exact);
+    const double erro_oh2 = std::abs(forwardOh2.calculate(quadratic, xValue, hValue) - exact);
+    CHECK(erro_oh2 < erro_oh);
+}
